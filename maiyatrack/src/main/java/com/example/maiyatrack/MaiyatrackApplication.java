77116571846bcc5ed5578d66aeb865f842d13612
Fengqi Jia <fjia0419@uni.sydney.edu.au>
2024-11-03 21:11:23 +1100

    Upload
    upload a spring boot web project file.

---
+++ maiyatrack/src/main/java/com/example/maiyatrack/MaiyatrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.maiyatrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaiyatrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaiyatrackApplication.class, args);
+	}
+
+}
---
+++ maiyatrack/src/test/java/com/example/maiyatrack/MaiyatrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maiyatrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaiyatrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
