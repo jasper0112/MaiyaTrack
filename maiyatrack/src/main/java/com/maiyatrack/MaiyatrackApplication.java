f3650508bab78d91b0fbb599238951ebfd7663be
Fengqi Jia <fjia0419@uni.sydney.edu.au>
2025-04-08 18:28:29 +1000

    delete something empty file

--- maiyatrack/src/main/java/com/maiyatrack/MaiyatrackApplication.java
+++ maiyatrack/src/main/java/com/maiyatrack/MaiyatrackApplication.java
@@ -1,8 +1,7 @@
-package com.example.maiyatrack;
+package com.maiyatrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
